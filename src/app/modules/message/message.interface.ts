0b71387d6cc300df21fa5bdd8e939cea3a6993aa
mayesha6 <soumyrhmn234@gmail.com>
2026-03-10 10:06:15 +0600

    message

--- src/app/modules/message/message.interface.ts
+++ src/app/modules/message/message.interface.ts
@@ -1,14 +1,8 @@
-export enum MessageSender {
-  USER = "USER",
-  BOT = "BOT",
-}
-
 export interface IMessage {
   _id?: string;
   conversationId: string;
-  sender: MessageSender;
+  role: "user" | "assistant";
   content: string;
-  fileId?: string; 
   createdAt?: Date;
   updatedAt?: Date;
 }
